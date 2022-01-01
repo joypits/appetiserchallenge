835484c5aa7b9bbb298ea32cf59f3373da54ade6
icto-laravel <icto.laravel.dev@gmail.com>
2022-01-01 13:27:37 +0800

    added api controller comments

--- app/Http/Controllers/Api/CalendarController.php
+++ app/Http/Controllers/Api/CalendarController.php
@@ -14,8 +14,8 @@ use DateInterval;
 class CalendarController extends Controller
 {
     public function showEvent(){
-        $event = Calendar::all();
-        $heada = Calendar::take(1)->get();
+        $event = Calendar::all(); //get all the calendar event record
+        $heada = Calendar::take(1)->get(); //take only 1 record from calendar event
         return Response::json(array(
             'event' => $event,
             'heada' => $heada,
@@ -23,7 +23,7 @@ class CalendarController extends Controller
     }
 
     public function addEvent(Request $request) {
-        $period = new DatePeriod(new DateTime($request->date_from), new DateInterval('P1D'), new DateTime($request->date_to.' +1 day'));
+        $period = new DatePeriod(new DateTime($request->date_from), new DateInterval('P1D'), new DateTime($request->date_to.' +1 day')); // get the date year, month and day from day 1 to day end of specific date range
         foreach ($period as $date) {
             $dates = $date->format("Y-m-d");
             $day = $date->format("d");
@@ -36,11 +36,11 @@ class CalendarController extends Controller
         }
 
         if($request->has('checkboxval')){
-            Calendar::whereIn('day',$request->checkboxval)->update(['event' =>  $request->event]);
-            Calendar::whereNotIn('day',$request->checkboxval)->update(['event' =>  null]);
+            Calendar::whereIn('day',$request->checkboxval)->update(['event' =>  $request->event]); //check and update event if calendar day is present from the checkbox value
+            Calendar::whereNotIn('day',$request->checkboxval)->update(['event' =>  null]); // check and update event to null if calendar day is not present from the checkbox value
         }
         else{
-            Calendar::whereBetween('date', [$request->date_from,$request->date_to])->update(['event' => null]);
+            Calendar::whereBetween('date', [$request->date_from,$request->date_to])->update(['event' => null]); // check and update event to null if checkbox value is null on the specific date range
         }
         
         return Response::json("success");
